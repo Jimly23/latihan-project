b79bdfc8e9cf687a93a9791b000fe2aeb96149c0
Jimly23 <jimlyasidqi692@gmail.com>
2023-10-11 09:12:30 +0700

    Menambahkan fitur salam

--- login.py
+++ login.py
@@ -4,12 +4,17 @@ class Login:
         self.email = email
         self.password = password
 
+    def info(self):
+        return f"Hallo selamat pagi/siang/sore Mas {self.username}"
+
     def show(self):
         return f"Username : {self.username}\nEmail : {self.email}\nPassword : {self.password}"
+    
 
 
 username = input("Input your username : ")
 email = input("Input your email : ")
 password = input("Input your password : ")
 user = Login(username,email,password)
+print(user.info())
 print(user.show())
